6971ee75b959230d2b428036661193f986a48af8
darpok_boi <deepanshu1297@gmail.com>
2024-11-25 17:56:19 +0530

    Updates

---
+++ precache-manifest.8c7e516f5f0c30ae4bc04b6f6954db4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61cbf0264a7db62478f6c22a0652e306",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "6f61ff921902bf44fd92",
+    "url": "/calculator/static/css/main.93cdd93f.chunk.css"
+  },
+  {
+    "revision": "d5254da0430cb348eda0",
+    "url": "/calculator/static/js/2.bb355b49.chunk.js"
+  },
+  {
+    "revision": "6f61ff921902bf44fd92",
+    "url": "/calculator/static/js/main.0c971c90.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  }
+]);
